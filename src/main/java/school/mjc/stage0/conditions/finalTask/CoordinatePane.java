eecb091437d205118f4461b339eb186a41ce6d1c
zanz1bra <talbergaerika@gmail.com>
2022-11-16 15:26:57 +0200

    1st try

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -5,11 +5,11 @@ public class CoordinatePane {
         if (x > 0 && y > 0) {
             System.out.println("first");
         } else if (x > 0 && y < 0) {
-            System.out.println("second");
+            System.out.println("fourth");
         } else if (x < 0 && y < 0) {
             System.out.println("third");
         } else if (x < 0 && y > 0) {
-            System.out.println("fourth");
+            System.out.println("second");
         } else {
             System.out.println("zero");
         }
