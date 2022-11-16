0c050fd12a34cf48576e53c79968b24c005b1b83
zanz1bra <talbergaerika@gmail.com>
2022-11-16 15:41:39 +0200

    1st try

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if ((firstSide + secondSide > thirdSide) || (firstSide + thirdSide > secondSide) || (secondSide + thirdSide > firstSide)) {
+        if ((firstSide + secondSide >= thirdSide) || (firstSide + thirdSide >= secondSide) || (secondSide + thirdSide >= firstSide)) {
             System.out.println("this is a valid triangle");
         } else {
             System.out.println("it's not a triangle");
